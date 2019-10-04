8ef9dc16099acfba84afcb2ae5d77f3860674afc
22ghoshi <ishaanghosh10@gmail.com>
2019-10-04 16:35:52 -0400

    lift + all auton functions go in opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -13,13 +13,43 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor backleft_mtr(4);
+pros::Motor backright_mtr(6);
+pros::Motor frontleft_mtr(7);
+pros::Motor frontright_mtr(5);
+pros::Motor angler(8);
+pros::Motor lift(9);
+pros::Motor left_intake(10);
+pros::Motor right_intake(11);
+
+//AUTON FUNCTIONS
+void move()
+{
+	//PID stuff
+}
+void rightTurn(int turn)
+{
+	frontright_mtr.move_absolute(-turn, 100);
+	backright_mtr.move_absolute(-turn, 100);
+	frontleft_mtr.move_absolute(turn, 100);
+	backright_mtr.move_absolute(turn, 100);
+}
+
+void leftTurn(int turn)
+{
+	frontright_mtr.move_absolute(turn, 100);
+	backright_mtr.move_absolute(turn, 100);
+	frontleft_mtr.move_absolute(-turn, 100);
+	backright_mtr.move_absolute(-turn, 100);
+}
+
+
+//DRIVER CONTROL
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor backleft_mtr(3);
-	pros::Motor backright_mtr(4);
-	pros::Motor frontleft_mtr(1);
-	pros::Motor frontright_mtr(2);
-	pros::Motor lift(5);
+
+
 	while (true) {
 		
 		int left = master.get_analog(ANALOG_LEFT_Y);
@@ -31,30 +61,58 @@ void opcontrol() {
 		}
 		else
 		{
-			frontleft_mtr = 0;
-			backleft_mtr = 0;
+			backright_mtr = 0;
+			frontright_mtr = 0;
+			frontleft_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			backleft_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 		}
 		
 		if (right > 15 || right < -15)
 		{
-			backright_mtr = right;
-			frontright_mtr = right;
+			backright_mtr = -right;
+			frontright_mtr = -right;
 		}
 		else
 		{
 			backright_mtr = 0;
 			frontright_mtr = 0;
+			backright_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			frontright_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 		}
-		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+		
+		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
 		{
-			lift = -30;
+			lift = -50;
+		}
+		else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+		{
+			lift = 100;
 		}
 		else
 		{
-			lift = 0;
+			lift = 15; //gives motor so little power that it can't actually move the lift
+			lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); //doesn't work?
 		}
+
+		/*need to set two positions of angler
+		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+		{
+			angler = 70;
+		}
+		else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_B))
+		{
+			angler = -70;
+		}
+		else
+		{
+			angler = 0;
+		}*/
 		
 
+		
+		
+		
+		pros::delay(20);
 
 	}
 }
